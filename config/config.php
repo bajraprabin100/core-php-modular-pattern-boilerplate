87b3a399dce4af687f29b1dc197af74d1a9c5ea6
Prabin Bajracharya <bajraprabin101@gmail.com>
2024-06-01 22:57:15 +0545

    Added Env file and added vlucas/phpdotenv package for env read

--- config/config.php
+++ config/config.php
@@ -1,12 +1,15 @@
 <?php
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 return [
+    'env' => $_ENV['ENVIRONMENT'],
     'db' => [
-        'host' => 'localhost',
-        'dbname' => 'intujiassignment',
-        'user' => 'root',
-        'password' => '',
+        'host' => $_ENV['DB_HOST'],
+        'dbname' => $_ENV['DB_NAME'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
     ],
-    'client_secret_path' => __DIR__ . '/credentials.json',
-    'redirect_uri' => 'http://localhost:8000/oauth2callback.php',
+    'client_secret_path' => __DIR__ . '/'. $_ENV['GOOGLE_CALENDER_FILE_NAME'],
+    'redirect_uri' => $_ENV['GOOGLE_OAUTH_REDIRECT_URI'],
 ];
 ?>
