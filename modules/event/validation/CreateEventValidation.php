14c8d2e0fa1f362b6e6b2dec7b4b90845dfe26b2
Prabin Bajracharya <bajraprabin101@gmail.com>
2024-06-03 13:41:26 +0545

    remove comments and double created methods

--- modules/event/validation/CreateEventValidation.php
+++ modules/event/validation/CreateEventValidation.php
@@ -6,22 +6,18 @@ class   CreateEventValidation
     {
         $errors = [];
 
-        // Check if summary is provided
         if (empty($data['summary'])) {
             $errors[] = "Please enter the event summary";
         }
 
-        // Check if startDateTime is provided and in the correct format
         if (empty($data['startDateTime']) || !strtotime($data['startDateTime'])) {
             $errors[] = "Please enter a valid start date and time";
         }
 
-        // Check if endDateTime is provided and in the correct format
         if (empty($data['endDateTime']) || !strtotime($data['endDateTime'])) {
             $errors[] = "Please enter a valid end date and time";
         }
 
-        // Check if endDateTime is after startDateTime
         if (!empty($data['startDateTime']) && !empty($data['endDateTime'])) {
             if (strtotime($data['startDateTime']) >= strtotime($data['endDateTime'])) {
                 $errors[] = "End date and time must be greater than start date and time";
